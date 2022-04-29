52e9e49776ee106ecc688a13c832d9b79fc81332
NachtaktiverHalbaffe <{e-A17XEv%q7U&>
2022-04-29 14:44:28 +0200

    Added validation to verse

--- data/interfaces/verse_interface.py
+++ data/interfaces/verse_interface.py
@@ -4,89 +4,20 @@ from data.models.verse import Verse
 
 
 class VerseInterface(ABC):
-    """A bible verse interface. It defines how the data of the verse is obtained and grants access to the verse"""
-
-    def __init__(self, book="", chapter=0, verse=0):
-        self.verse = Verse(book=book, chapter=chapter, verse=verse)
+    """A bible verse interface. It defines how the data of the verse is collected"""
 
     @abstractmethod
-    async def loadData(book="", chapter=0, verse=0) -> None:
-        """Load the verse itself from the datasource. It uses the chapter, verse and book property of the class 
-        itself to search for the verse. Attention: It only gets the verse itself, for the metadata see at the
-        function loadMetaData.
+    async def collectVerse(book: str, chapter: int, verse: int) -> Verse:
+        """Collect the verse itself from the datasource. 
 
         Args:
-            book (str, optional): Name of the correpsonding book. Defaults to "".
-            chapter (int, optional): Number of the correpsonding chapter. Defaults to 0.
-            verse (int, optional): Verse number to load. Defaults to 0.
+            book (str): Name of the correpsonding book. Defaults to "".
+            chapter (int): Number of the correpsonding chapter. Defaults to 0.
+            verse (int): Verse number to load. Defaults to 0.
         """
         pass
 
     @abstractmethod
-    async def loadMetaData() -> None:
-        """Load the metadata of the verse from the datasource"""
+    async def collectMetaData(verse: Verse) -> Verse:
+        """Collect the metadata of the verse from the datasource"""
         pass
-
-    def getContent(self) -> str:
-        """Returns the verse itself
-
-        Returns:
-            str: The verse itself
-        """
-        return self.verse.data
-
-    def getVerse(self) -> int:
-        """Returns the verse number of the verse
-
-        Returns:
-            int: Number of the verse
-        """
-        
-        return self.verse.verse
-
-    def getChapter(self) -> int:
-        """Returns the corresponding chapter of the verse
-
-        Returns:
-            int: Number of chapter where the verse belongs to
-        """
-        
-        return self.verse.chapter
-
-    def getAuthor(self) -> str:
-        """Returns the corresponding author of the verse
-
-        Returns:
-            str: Name of author where the verse belongs to
-        """
-        return self.verse.author
-
-    def getBook(self) -> str:
-        """Returns the corresponding book of the verse
-
-        Returns:
-            str:  Name of book where the verse belongs to
-        """
-        
-        return self.verse.book
-
-    def getMetaData(self) -> dict:
-        """Returns the meta data as a dictionary which only contains entries which have a explicit entry
-
-        Returns:
-            dict: metaData with only the values which contains data
-        """
-
-        payload = dict()
-        if self.verse.verse != 0:
-            payload["verse"] = self.verse.verse
-        if self.verse.chapter != 0:
-            payload["chapter"] = self.verse.chapter
-        if self.verse.book != "":
-            payload["book"] = self.verse.book
-        if self.verse.author != "":
-            payload["author"] = self.verse.author
-        if self.verse.wordCnt != 0:
-            payload["wordCnt"] = self.verse.wordCnt
-
-        return payload
--- data/models/verse.py
+++ data/models/verse.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass, field
+import logging
+import sys
+
+from utils.constantLoader import ConstantLoader
 
 
 @dataclass(slots=True)
 class Verse:
-    """Models a Bible verse. The verse itself is divided into the verse itself and the metadata. Because
-    the verse can be a too fine-granular unit or can be redundant with the chapter meta-data, the meta
-    data itself is optional, but if you want to load the verse the book, chapter and verse properties are
-    mandatory. To minimize data size, you can set the metaData to None after you loaded the verse itself
+    """Models a Bible verse. This is the only data model containing real data
     """
 
     book: str = ""
@@ -17,11 +18,105 @@ class Verse:
     """Number of the verse itself"""
     wordCnt: int = 0
     """Number of verses in the chapter"""
-    author: str = ""
-    """Author of the book where the verse belongs to"""
-    tags: list[str] = field(default_factory=list)
-    """Tags of the verse. Tags are buzzwords that can categorize a chapter i.e. tags can give some further information 
-    about the verse e.g. a tag \"Popular\" if its a popular verse like Joh 3,16d. To be efficiently analyzed a tag from a 
-    constant tag pool should be used"""
-    data: str = ""
+    content: str = ""
     """Verse itself """
+
+    def __init__(self) -> None:
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s [%(levelname)s] %(message)s",
+            handlers=[
+                logging.FileHandler("/logs/data.log"),
+                logging.StreamHandler(sys.stdout)
+            ]
+        )
+
+    def __post__init__(self) -> None:
+        if self.content != "" and self.wordCnt != 0:
+            self.wordCnt = len(self.content.split(" "))
+
+    def getMetaData(self) -> dict:
+        """Returns the meta data as a dictionary which only contains entries which have a explicit entry
+
+        Returns:
+            dict: metaData with only the values which contains data
+        """
+
+        payload = dict()
+        if self.verse != 0:
+            payload["verse"] = self.verse
+        if self.chapter != 0:
+            payload["chapter"] = self.chapter
+        if self.book != "":
+            payload["book"] = self.book
+        if self.wordCnt != 0:
+            payload["wordCnt"] = self.wordCnt
+        if self.chapter != "":
+            payload["content"] = self.content
+
+        return payload
+
+    def validateData(self) -> bool:
+        """Validates the data to avoid having false data
+
+        Returns:
+            bool: If data is valid (true) or has fault (false)
+        """
+
+        # Book
+        if type(self.book) != type(str):
+            logging.warning(
+                f"Property book has wrong type: {type(self.book)}. Expected type: str")
+            return False
+        elif not self.book in ConstantLoader().getConstants("books"):
+            logging.warning(
+                f"{self.book} is not defined as a book in the constant pool ")
+            return False
+
+        # Chapter
+        if type(self.chapter) != type(int):
+            logging.warning(
+                f"Property chapter has wrong type: {type(self.chapter)}. Expected type: int")
+            return False
+        elif self.chapter < 0 or self.chapter > 150:
+            logging.warning(
+                "Property chapter is either too big or too small. There are only 150 chapters in the biggest book of the bible")
+            # Value out o bounds (the biggest book has 150 chapters)
+            return False
+
+        # Verse
+        if type(self.verse) != type(int):
+            logging.warning(
+                f"Property verse has wrong type: {type(self.verse)}. Expected type: int")
+            return False
+        elif self.verse < 0:
+            logging.warning("Property verse is either too small or too big")
+            return False
+
+        # wordCnt
+        if type(self.wordCnt) != type(int):
+            logging.warning(
+                f"Property wordCnt has wrong type: {type(self.wordCnt)}. Expected type: int")
+            return False
+        elif self.wordCnt < 0:
+            logging.warning("Property wordCnt is negative")
+            return False
+
+        # tags
+        for tag in self.tags:
+            if type(tag) != type(str):
+                logging.warning(
+                    f"Entry in property tags has wrong type: {type(tag)}. Expected type: str")
+                return False
+            elif not tag in ConstantLoader().getConstants("tags"):
+                logging.warning(
+                    f"{tag} is not defined as a tag in the constant pool ")
+                return False
+
+        # content
+        if type(self.content) != type(str):
+            logging.warning(
+                f"Property content has wrong type: {type(self.content)}. Expected type: str")
+            return False
+
+        return True
